5b1f16c33c6b9d55833c07424d54717a9700337f
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-30 16:40:18 -0400

    fix include

--- hazelform/hazelform.h
+++ hazelform/hazelform.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include QMK_KEYBOARD_H
+#include "quantum.h"
 
 #define	___	KC_NO
 
-#define LAYOUT_3x5(\
+#define LAYOUT_3x5( \
 	K01, K02, K03, K04, K05, \
 	K06, K07, K08, K09, K10, \
 	K11, K12, K13, K14, K15  \
