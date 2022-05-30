e57eb92a2695bfd1b309ebe948e00d69df0a68bf
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-30 16:16:37 -0400

    rules.mk

---
+++ hazelform/rules.mk
@@ -0,0 +1,13 @@
+LTO_ENABLE 						= yes
+KEY_OVERRIDE_ENABLE		= yes
+COMBO_ENABLE 					= yes
+TAP_DANCE_ENABLE 			= yes
+KEYCOUNTER_ENABLE			= yes
+CAPS_WORD_ENABLE 			= yes
+
+# disable stuff to save space
+GRAVE_ESC_ENABLE 			= no
+MUSIC_ENABLE 					= no
+MAGIC_ENABLE 					= no
+SPACE_CADET_ENABLE 		= no
+MOUSEKEY_ENABLE 			= no
