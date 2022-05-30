0c083543715b375ce55b5757224f5e0646e3c0cf
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-30 16:48:12 -0400

    fix cols

--- hazelform/config.h
+++ hazelform/config.h
@@ -12,7 +12,7 @@
 #define MATRIX_ROWS		3
 #define MATRIX_COLS		5
 
-#define MATRIX_ROW_PINS { B5, B4, E6 }
+#define MATRIX_ROW_PINS { E6, B4, B5 }
 #define MATRIX_COL_PINS { B6, B2, B3, B1, F7 }
 
 #define DIODE_DIRECTION COL2ROW
